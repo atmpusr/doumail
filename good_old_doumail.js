d3bb7d3d8c1651a1992bf13db16244edd5b0a0f7
atmpusr <heavensimple@gmail.com>
2015-05-17 22:56:50 +0200

    Update good_old_doumail.js

--- good_old_doumail.js
+++ good_old_doumail.js
@@ -8,19 +8,19 @@
 // ==/UserScript==
 
 // 页面标题
-document.title = document.title.replace('私信','我的豆邮');
+document.title = document.title.replace('私信','我的豆邮')
 
 // 导航
-var var_nav = $('#top-nav-doumail-link')[0];
-var_nav.innerHTML = var_nav.innerHTML.replace('私信','豆邮');
+var var_nav = $('#top-nav-doumail-link')[0]
+var_nav.innerHTML = var_nav.innerHTML.replace('私信','豆邮')
 
 // 删除提示条，替换标题
 $(".pop").remove()
-var var_header =$('#content h1')[0];
+var var_header =$('#content h1')[0]
 var_header.innerHTML = var_header.innerHTML.replace('私信','我的豆邮')
 
 // 剩余内容
 var var_list = $("a")
 for(var i = 0;i < var_list.length; i++) {
-  var_list[i].innerHTML = var_list[i].innerHTML.replace('私信', '豆邮');
+  var_list[i].innerHTML = var_list[i].innerHTML.replace('私信', '豆邮')
 }
